84c13937a8f44a687162f266cb3020745cb0700d
ShamReh <sr.166@hotmail.co.uk>
2021-11-01 17:46:26 +0000

    Correction

--- dist/src/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-let age;
-age = 30;
-const userName = 'Shamshur';
-console.log(userName);
-const button = document.querySelector('button');
-button.addEventListener('click', () => {
-    console.log('Clicked!');
-});
